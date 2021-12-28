c59804b0e20f8f637b1fcdeb1e01fd5aca34fa17
ken0327 <dr.k0327@gmail.com>
2021-12-28 23:30:34 +0800

    indexers

---
+++ Tutorial/OOProgramming/OOProgramming/Indexers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOProgramming
+{
+    class Indexers
+    {
+        class SampleCollection<T>
+            {
+                // Declare an array to store the data elements.
+                private T[] arr = new T[100];
+
+                // Define the indexer to allow client code to use [] notation.
+                public T this[int i]
+                {
+                    get { return arr[i]; }
+                    set { arr[i] = value; }
+                }
+            }
+
+            class Program
+            {
+                static void Execute()
+                {
+                    var stringCollection = new SampleCollection<string>();
+                    stringCollection[0] = "Hello, World";
+                    Console.WriteLine(stringCollection[0]);
+                }
+            }
+            // The example displays the following output:
+            //       Hello, World.
+        }
+}
