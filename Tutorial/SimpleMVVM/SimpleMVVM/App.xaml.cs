a2c67b29d467dbce61c76143b98d2c5bd62697db
ken0327 <dr.k0327@gmail.com>
2022-02-06 17:58:26 +0800

    Add Simple MVVM project

---
+++ Tutorial/SimpleMVVM/SimpleMVVM/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace SimpleMVVM
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    base.OnStartup(e);
+        //    MainWindow window = new MainWindow();
+        //    UserViewModel VM = new UserViewModel();
+        //    window.DataContext = VM;
+        //    window.Show();
+        //}
+    }
+}
---
+++ Tutorial/SimpleMVVM/SimpleMVVM/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace SimpleMVVM
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ModelViewBinding();
+        }
+
+        private void ModelViewBinding()
+        {
+            UserViewModel VM = new UserViewModel();
+            this.DataContext = VM;
+        }
+    }
+}
